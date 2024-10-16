f4b34fd202208c5cfb25615a5bc4c44850f345e7
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-10-16 11:24:37 +0100

    Change struct back to class

--- HitScoreVisualizer/Settings/NormalJudgment.cs
+++ HitScoreVisualizer/Settings/NormalJudgment.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 namespace HitScoreVisualizer.Settings
 {
 	[method: JsonConstructor]
-	public readonly struct NormalJudgment(int threshold = 0, string? text = null, List<float>? color = null, bool fade = false)
+	public class NormalJudgment(int threshold = 0, string? text = null, List<float>? color = null, bool fade = false)
 	{
 		[JsonIgnore]
 		internal static NormalJudgment Default { get; } = new(0, "%s", [1, 1, 1, 1], false);
