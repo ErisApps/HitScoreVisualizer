6d7d0eacbf345317b6c5e1e3eaababa891bbf1b2
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-10-30 13:57:14 +0000

    Revert primary constructors

--- HitScoreVisualizer/Settings/JudgmentSegment.cs
+++ HitScoreVisualizer/Settings/JudgmentSegment.cs
@@ -2,19 +2,25 @@ using Newtonsoft.Json;
 
 namespace HitScoreVisualizer.Settings
 {
-	[method: JsonConstructor]
-	public class JudgmentSegment(int threshold = 0, string? text = null)
+	public class JudgmentSegment
 	{
+		[JsonConstructor]
+		public JudgmentSegment(int threshold = 0, string? text = null)
+		{
+			Threshold = threshold;
+			Text = text ?? string.Empty;
+		}
+
 		[JsonIgnore]
 		internal static JudgmentSegment Default { get; } = new(0, string.Empty);
 
 		// This judgment will be applied only when the appropriate part of the swing contributes score >= this number.
 		// If no judgment can be applied, the judgment for this segment will be "" (the empty string).
 		[JsonProperty("threshold")]
-		public int Threshold { get; } = threshold;
+		public int Threshold { get; }
 
 		// The text to replace the appropriate judgment specifier with (%B, %C, %A) when this judgment applies.
 		[JsonProperty("text")]
-		public string? Text { get; } = text ?? string.Empty;
+		public string? Text { get; }
 	}
 }
--- HitScoreVisualizer/Settings/TimeDependenceJudgmentSegment.cs
+++ HitScoreVisualizer/Settings/TimeDependenceJudgmentSegment.cs
@@ -2,19 +2,25 @@ using Newtonsoft.Json;
 
 namespace HitScoreVisualizer.Settings
 {
-	[method: JsonConstructor]
-	public class TimeDependenceJudgmentSegment(float threshold = 0f, string? text = null)
+	public class TimeDependenceJudgmentSegment
 	{
+		[JsonConstructor]
+		public TimeDependenceJudgmentSegment(float threshold = 0f, string? text = null)
+		{
+			Threshold = threshold;
+			Text = text ?? string.Empty;
+		}
+
 		[JsonIgnore]
 		internal static TimeDependenceJudgmentSegment Default { get; } = new(0, string.Empty);
 
 		// This judgment will be applied only when the time dependence >= this number.
 		// If no judgment can be applied, the judgment for this segment will be "" (the empty string).
 		[JsonProperty("threshold")]
-		public float Threshold { get; } = threshold;
+		public float Threshold { get; }
 
 		// The text to replace the appropriate judgment specifier with (%T) when this judgment applies.
 		[JsonProperty("text")]
-		public string? Text { get; } = text ?? string.Empty;
+		public string? Text { get; }
 	}
 }
