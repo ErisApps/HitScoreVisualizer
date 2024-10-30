20f0398a29367076ad35c1ba3458b82707c803a4
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-10-30 13:57:14 +0000

    Revert primary constructors

--- HitScoreVisualizer/Settings/ChainLinkDisplay.cs
+++ HitScoreVisualizer/Settings/ChainLinkDisplay.cs
@@ -3,19 +3,25 @@ using Newtonsoft.Json;
 
 namespace HitScoreVisualizer.Settings
 {
-	[method: JsonConstructor]
-	public class ChainLinkDisplay(string? text = null, List<float>? color = null)
+	public class ChainLinkDisplay
 	{
+		[JsonConstructor]
+		public ChainLinkDisplay(string? text = null, List<float>? color = null)
+		{
+			Text = text ?? string.Empty;
+			Color = color ?? [0, 0, 0, 0];
+		}
+
 		[JsonIgnore]
 		internal static ChainLinkDisplay Default { get; } = new("<u>20", [1, 1, 1, 1]);
 
 		// The text to display for a chain segment
 		[JsonProperty("text")]
-		public string Text { get; } = text ?? string.Empty;
+		public string Text { get; }
 
 		// 4 floats, 0-1; red, green, blue, glow (not transparency!)
 		// leaving this out should look obviously wrong
 		[JsonProperty("color")]
-		public List<float> Color { get; } = color ?? [0, 0, 0, 0];
+		public List<float> Color { get; }
 	}
 }
