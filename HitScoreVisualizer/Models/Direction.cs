416cff07f649d67a83713c5b4ab41147f03c1adf
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-10-31 09:39:08 +0000

    Add direction token

---
+++ HitScoreVisualizer/Models/Direction.cs
@@ -0,0 +1,14 @@
+namespace HitScoreVisualizer.Models;
+
+public enum Direction
+{
+	Up = 0,
+	UpRight = 1,
+	Right = 2,
+	DownRight = 3,
+	Down = 4,
+	DownLeft = 5,
+	Left = 6,
+	UpLeft = 7,
+	None,
+}
