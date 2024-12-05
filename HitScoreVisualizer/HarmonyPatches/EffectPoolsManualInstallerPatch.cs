77681cc3e0b2ce5b153bde4e5b45f716a2860b89
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-12-05 15:24:22 +0000

    Change Enable Italics to Disable

--- HitScoreVisualizer/HarmonyPatches/EffectPoolsManualInstallerPatch.cs
+++ HitScoreVisualizer/HarmonyPatches/EffectPoolsManualInstallerPatch.cs
@@ -27,12 +27,8 @@ namespace HitScoreVisualizer.HarmonyPatches
 			text.overflowMode = TextOverflowModes.Overflow;
 
 			// Configure font shader and italics
-			text.font = hsvConfig.HitScoreBloom
-				? bloomFontProvider.BloomFont
-				: bloomFontProvider.DefaultFont;
-			text.fontStyle = hsvConfig.EnableItalics
-				? FontStyles.Italic
-				: FontStyles.Normal;
+			text.font = hsvConfig.HitScoreBloom ? bloomFontProvider.BloomFont : bloomFontProvider.DefaultFont;
+			text.fontStyle = hsvConfig.DisableItalics ? FontStyles.Normal : FontStyles.Italic;
 		}
 	}
 }
