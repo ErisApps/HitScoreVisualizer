1ee6fb80c70a778669758d4da7dedc04e1f4af88
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-12-05 15:24:49 +0000

    Extract direction to its class

--- HitScoreVisualizer/Extensions/DirectionConversion.cs
+++ HitScoreVisualizer/Extensions/DirectionConversion.cs
@@ -1,9 +1,34 @@
+using System;
+using System.Collections.Generic;
 using HitScoreVisualizer.Models;
+using IPA.Utilities;
+using UnityEngine;
 
 namespace HitScoreVisualizer.Extensions;
 
 internal static class DirectionConversion
 {
+	private static readonly float Angle = Mathf.Sqrt(2) / 2;
+
+	private static readonly Dictionary<Direction, Vector3> NormalsMap = new()
+	{
+		{ Direction.Down, new(0, -1, 0) },
+		{ Direction.DownLeft, new(-Angle, -Angle, 0) },
+		{ Direction.Left, new(-1, 0, 0) },
+		{ Direction.UpLeft, new(-Angle, Angle, 0)}
+	};
+
+	public static Direction CalculateOffDirection(this NoteCutInfo noteCutInfo)
+	{
+		var direction = GetClosestOffDirection(noteCutInfo.cutNormal);
+		var directionAsInt = (int)direction;
+		return
+			direction == Direction.None ? direction
+			: Vector3.Dot(NormalsMap[direction], noteCutInfo.notePosition - noteCutInfo.cutPoint) > 0 ? direction
+			: directionAsInt < 4 ? (Direction)(directionAsInt + 4)
+			: (Direction)(directionAsInt - 4);
+	}
+
 	public static string ToFormattedDirection(this Direction direction)
 	{
 		return direction switch
@@ -18,5 +43,25 @@ internal static class DirectionConversion
 			Direction.UpLeft => "\u2196",
 			_ => string.Empty
 		};
+	}
+
+	private static Direction GetClosestOffDirection(Vector3 cutNormal)
+	{
+		var closestDot = Mathf.NegativeInfinity;
+		var result = Direction.None;
+
+		foreach (var (direction, normal) in NormalsMap)
+		{
+			var dot = Vector3.Dot(cutNormal, normal);
+			var dotValue = Math.Abs(dot);
+			if (!(dotValue > closestDot))
+			{
+				continue;
+			}
+			closestDot = dot;
+			result = direction;
+		}
+
+		return result;
 	}
 }
