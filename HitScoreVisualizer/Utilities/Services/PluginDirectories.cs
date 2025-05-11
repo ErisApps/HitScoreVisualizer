609ea4b88bb9651f151550267177465e38e4ddca
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2025-05-11 01:04:30 +0100

    Pull down migration logic and directories

---
+++ HitScoreVisualizer/Utilities/Services/PluginDirectories.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using IPA.Utilities;
+using Zenject;
+
+namespace HitScoreVisualizer.Utilities.Services;
+
+internal class PluginDirectories
+{
+	private readonly DirectoryInfo configs;
+	private readonly DirectoryInfo backups;
+
+	public PluginDirectories()
+	{
+		var configsPath = Path.Combine(UnityGame.UserDataPath, nameof(HitScoreVisualizer));
+		configs = new(configsPath);
+		backups = Configs.CreateSubdirectory("Backups");
+	}
+
+	public DirectoryInfo Configs => CreateDirectoryIfNotExists(configs);
+
+	public DirectoryInfo Backups => CreateDirectoryIfNotExists(backups);
+
+	private static DirectoryInfo CreateDirectoryIfNotExists(DirectoryInfo directory)
+	{
+		if (!directory.Exists)
+		{
+			directory.Create();
+		}
+		return directory;
+	}
+}
