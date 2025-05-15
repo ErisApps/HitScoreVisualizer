ef068549ffc91203a15a520e8536dd452a308b73
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2025-05-15 14:21:24 +0100

    Simplify logic by injecting the selected config

--- HitScoreVisualizer/Installers/HsvPlayerInstaller.cs
+++ HitScoreVisualizer/Installers/HsvPlayerInstaller.cs
@@ -6,10 +6,25 @@ namespace HitScoreVisualizer.Installers;
 
 internal class HsvPlayerInstaller : Installer
 {
+	private readonly PluginConfig pluginConfig;
 	private readonly HsvFlyingEffect hsvFlyingEffectPrefab = HsvFlyingEffect.CreatePrefab();
+
+	public HsvPlayerInstaller(PluginConfig pluginConfig)
+	{
+		this.pluginConfig = pluginConfig;
+	}
 
 	public override void InstallBindings()
 	{
+		var currentConfig = pluginConfig.SelectedConfig?.Config;
+		if (currentConfig is null)
+		{
+			// No valid HSV config is selected, so HSV will not do anything during gameplay
+			return;
+		}
+
+		Container.BindInstance(currentConfig).AsSingle();
+
 		Container.Bind<HsvFlyingEffectSpawner>().FromNewComponentOnNewGameObject().AsSingle();
 
 		Container.BindMemoryPool<HsvFlyingEffect, HsvFlyingEffect.Pool>()
