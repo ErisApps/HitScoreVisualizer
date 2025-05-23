dc657dcc901deab92238ea80921fbd8f33f20c31
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2025-05-23 15:57:15 +0100

    Fill in Custom Preview Tab elements

--- HitScoreVisualizer/UI/ConfigPreviewCustomTab.cs
+++ HitScoreVisualizer/UI/ConfigPreviewCustomTab.cs
@@ -1,10 +1,33 @@
+using System;
+using System.Text.RegularExpressions;
+using HitScoreVisualizer.Models;
+
 namespace HitScoreVisualizer.UI;
 
 internal class ConfigPreviewCustomTab
 {
+	private CustomPreviewTab currentTab;
+
+	public Array BadCutTypeOptions { get; set; } = Enum.GetValues(typeof(BadCutDisplayType));
+
+	public object BadCutTypeFormatter(object v)
+	{
+		return Regex.Replace(v.ToString(), "(\\B[A-Z])", " $1");
+	}
+
 	public void CustomPreviewTabChanged(object segmentedControl, int index)
 	{
+		currentTab = (CustomPreviewTab)index;
+	}
 
+	private BadCutDisplayType badCutType = BadCutDisplayType.All;
+	public BadCutDisplayType BadCutType
+	{
+		get => badCutType;
+		set
+		{
+			badCutType = value;
+		}
 	}
 
 	public void Disable()
@@ -13,5 +36,13 @@ internal class ConfigPreviewCustomTab
 
 	public void Enable()
 	{
+	}
+
+	private enum CustomPreviewTab
+	{
+		Judgments = 0,
+		ChainLink = 1,
+		BadCut = 2,
+		Miss = 3
 	}
 }
