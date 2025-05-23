b173d47b62c984a4edad4ef8915855276f3d9a56
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2025-05-23 13:36:20 +0100

    Add custom preview tab

---
+++ HitScoreVisualizer/UI/ConfigPreviewCustomTab.cs
@@ -0,0 +1,17 @@
+namespace HitScoreVisualizer.UI;
+
+internal class ConfigPreviewCustomTab
+{
+	public void CustomPreviewTabChanged(object segmentedControl, int index)
+	{
+
+	}
+
+	public void Disable()
+	{
+	}
+
+	public void Enable()
+	{
+	}
+}
