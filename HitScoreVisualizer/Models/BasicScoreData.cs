fc5d248764b23bc9ae3ff6f779539daa2ad67528
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2025-05-23 01:07:00 +0100

    Grid preview cleanup

---
+++ HitScoreVisualizer/Models/BasicScoreData.cs
@@ -0,0 +1,3 @@
+namespace HitScoreVisualizer.Models;
+
+internal record BasicScoreData(int Before, int Center, int After);
--- HitScoreVisualizer/UI/ConfigPreviewGridTab.cs
+++ HitScoreVisualizer/UI/ConfigPreviewGridTab.cs
@@ -1,9 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using BeatSaberMarkupLanguage.Attributes;
 using HitScoreVisualizer.Models;
-using HitScoreVisualizer.Utilities.Extensions;
 using HitScoreVisualizer.Utilities.Services;
 using TMPro;
 using UnityEngine;
@@ -21,6 +19,16 @@ internal class ConfigPreviewGridTab
 
 	[UIComponent("ScoreGrid")] private readonly GridLayoutGroup scoreGrid = null!;
 	[UIObject("GridTextTemplate")] private readonly GameObject gridTextTemplate = null!;
+	private readonly BasicScoreData[] gridScores =
+	[
+		new(70, 15, 30), new(70, 14, 30), new(70, 13, 30), new(70, 12, 30),
+		new(70, 11, 30), new(70, 10, 30), new(70, 05, 30), new(70, 00, 30),
+		new(61, 15, 26), new(52, 10, 22), new(43, 05, 18), new(34, 00, 14),
+		new(26, 15, 10), new(17, 10, 07), new(09, 05, 04), new(00, 00, 00)
+	];
+
+	private readonly WaitForSeconds animationInterval = new(0.04f);
+	private PreviewGridText[] gridTexts = null!; // assigned in initializer
 
 	[UIAction("#post-parse")]
 	public void PostParse()
@@ -28,6 +36,15 @@ internal class ConfigPreviewGridTab
 		scoreGrid.childAlignment = TextAnchor.MiddleCenter;
 		scoreGrid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
 		scoreGrid.constraintCount = 4;
+		gridTexts = gridScores.Select(score =>
+		{
+			var gameObject = Object.Instantiate(gridTextTemplate, scoreGrid.transform);
+			gameObject.name = nameof(PreviewGridText);
+			var textMesh = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+			textMesh.enableAutoSizing = true;
+			textMesh.fontSizeMax = 5f;
+			return new PreviewGridText(gameObject, textMesh, score);
+		}).ToArray();
 	}
 
 	public void Enable()
@@ -58,60 +75,20 @@ internal class ConfigPreviewGridTab
 		}
 	}
 
-	private readonly WaitForSeconds waitForSeconds = new(0.033f);
-	private readonly List<TextMeshProUGUI> gridTexts = [];
-
-	private record GridScore(int Before, int Center, int After);
-	private readonly GridScore[] gridScores =
-	[
-		new(70, 15, 30), new(70, 14, 30), new(70, 13, 30), new(70, 12, 30),
-		new(70, 11, 30), new(70, 10, 30), new(70, 05, 30), new(70, 00, 30),
-		new(61, 15, 26), new(52, 10, 22), new(43, 05, 18), new(34, 00, 14),
-		new(26, 15, 10), new(17, 10, 07), new(09, 05, 04), new(00, 00, 00)
-	];
-
 	private IEnumerator AnimateGridDisplay(HsvConfigModel config)
 	{
 		foreach (var text in gridTexts)
 		{
-			if (text != null)
-			{
-				text.gameObject.SetActive(false);
-			}
+			text.SetActive(false);
 		}
 
-		for (var i = 0; i < gridScores.Length; i++)
+		foreach (var text in gridTexts)
 		{
-			var (before, center, after) = gridScores[i];
-			var textMesh = gridTexts.ElementAtOrDefault(i);
-			if (textMesh == null)
-			{
-				var gameObject = Object.Instantiate(gridTextTemplate, scoreGrid.transform);
-				gameObject.name = $"PreviewGridScore {i}";
-				gameObject.SetActive(true);
-				textMesh = gameObject.GetComponentInChildren<TextMeshProUGUI>();
-				gridTexts.Add(textMesh);
-			}
-			textMesh.gameObject.SetActive(true);
-			textMesh.enableAutoSizing = true;
-			textMesh.fontSizeMax = 5;
-			textMesh.fontStyle = pluginConfig.DisableItalics ? FontStyles.Normal : FontStyles.Italic;
-			var totalCutScore = before + center + after;
-			var judgmentDetails = new JudgmentDetails
-			{
-				BeforeCutScore = before,
-				CenterCutScore = center,
-				AfterCutScore = after,
-				MaxPossibleScore = 115,
-				TotalCutScore = totalCutScore,
-				CutInfo = new(
-					NoteData.CreateBasicNoteData(0, 0, 0, 0, 0, 0, 0),
-					true, true, true, false, 0, Vector3.zero, SaberType.SaberA, 0, 0, Vector3.zero, Vector3.zero, 0, 0, Quaternion.identity, Quaternion.identity, Quaternion.identity, Vector3.zero,
-					new SaberMovementData())
-			};
-			(textMesh.text, textMesh.color) = config.Judge(in judgmentDetails);
+			text.SetActive(true);
+			text.FontStyle = pluginConfig.DisableItalics ? FontStyles.Normal : FontStyles.Italic;
+			text.SetTextForConfig(config);
 
-			yield return waitForSeconds;
+			yield return animationInterval;
 		}
 	}
 }
